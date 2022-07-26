d004a509c72111540124d3118a34aed12a042f3b
Lior Zemah <liorz@cynet.com>
2022-07-26 15:51:40 +0300

    add bash scripts to install falcolibs

---
+++ scripts/acquire_falcolibs_driver.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# switch to gcc 4.4.7 
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin 
+cd /home
+
+# yum -y install kernel-devel-$(uname -r)
+
+if [ -d "/home/LuaJIT" ] 
+then
+    echo "LuaJIT already installed." 
+else
+    echo "Cloning LuaJIT" 
+    git clone https://github.com/LuaJIT/LuaJIT.git
+fi 
+
+
+if [ -d "/home/falcolibs" ] 
+then
+    echo "/home/falcolibs directory already exists." 
+else
+    echo "Cloning Falcolibs directory" 
+    #rm -rf falcolibs/
+    wget https://github.com/falcosecurity/libs/archive/refs/tags/0.8.0.tar.gz
+    tar -xf 0.8.0.tar.gz
+    mv libs-0.8.0 falcolibs
+fi
+
+
+cd falcolibs || exit 1
+if [ ! -d "/home/falcolibs/build" ] 
+then
+    mkdir build
+fi
+
+cd build
+
+cmake -DUSE_BUNDLED_DEPS=true -DMINIMAL_BUILD=true -DCREATE_TEST_TARGETS=OFF -DWITH_CHISEL=true ../
+make driver
